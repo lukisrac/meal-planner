6b484fa29aa0e2e22793e212836ee86fa49ad015
Lukáš Rác <rac@uxf.cz>
2024-10-19 15:07:11 +0200

    refactored form

--- src/components/form/form/form.tsx
+++ src/components/form/form/form.tsx
@@ -1,42 +1,51 @@
+"use client";
+
 import { ReactNode } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form as ReactAriaForm } from "react-aria-components";
 import {
     FieldValues,
     FormProvider,
     SubmitErrorHandler,
     SubmitHandler,
+    useForm,
+    UseFormProps,
     UseFormReturn,
 } from "react-hook-form";
-import { FormIdContextProvider } from "@/providers/form-id-context";
+import { z, ZodType } from "zod";
 
-interface Props<TFieldValues extends FieldValues> {
-    children: ReactNode;
+interface Props<TFieldValues extends FieldValues, Schema> {
+    // eslint-disable-next-line no-unused-vars
+    children: (methods: UseFormReturn<TFieldValues>) => ReactNode;
     className?: string;
-    formApi: UseFormReturn<TFieldValues>;
-    handleError?: SubmitErrorHandler<TFieldValues>;
-    id: string;
-    method?: "get" | "post";
+    id?: string;
+    onHandleError?: SubmitErrorHandler<TFieldValues>;
     onSubmit: SubmitHandler<TFieldValues>;
+    options?: UseFormProps<TFieldValues>;
+    schema: Schema;
 }
 
-export function Form<TFieldValues extends FieldValues = FieldValues>(
-    props: Props<TFieldValues>,
-) {
+export function Form<
+    Schema extends ZodType<any, any, any>, // eslint-disable-line
+    TFieldValues extends FieldValues = z.infer<Schema>,
+>(props: Props<TFieldValues, Schema>) {
+    const form = useForm({
+        ...props.options,
+        resolver: zodResolver(props.schema),
+    });
+
     return (
-        <FormIdContextProvider value={props.id}>
-            <FormProvider {...props.formApi}>
-                <form
-                    className={props.className}
-                    id={props.id}
-                    method={props.method ?? "post"}
-                    noValidate
-                    onSubmit={props.formApi.handleSubmit(
-                        props.onSubmit,
-                        props.handleError,
-                    )}
-                >
-                    {props.children}
-                </form>
-            </FormProvider>
-        </FormIdContextProvider>
+        <FormProvider {...form}>
+            <ReactAriaForm
+                className={props.className}
+                id={props.id}
+                onSubmit={form.handleSubmit(
+                    props.onSubmit,
+                    props.onHandleError,
+                )}
+            >
+                {props.children(form)}
+            </ReactAriaForm>
+        </FormProvider>
     );
 }
--- src/components/forms/create-recipe-form.tsx
+++ src/components/forms/create-recipe-form.tsx
@@ -1,32 +1,22 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Form } from "react-aria-components";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { Button } from "react-aria-components";
+import { SubmitHandler } from "react-hook-form";
 import { z } from "zod";
+import { Form } from "@/components/form/form/form";
 import { NumberInput } from "@/components/form/number-input/number-input";
 import { TextInput } from "@/components/form/text-input/text-input";
 
 const FORM_SCHEMA = z.object({
     description: z.string().min(1, "Popisek je povinný"),
-    title: z.string().min(1, "Název je povinný"),
     preparationTime: z.number().min(1),
     servings: z.number().min(1),
+    title: z.string().min(1, "Název je povinný"),
 });
 
 type RecipeFormData = z.infer<typeof FORM_SCHEMA>;
 
 export function CreateRecipeForm() {
-    const formApi = useForm<RecipeFormData>({
-        resolver: zodResolver(FORM_SCHEMA),
-        defaultValues: {
-            title: "",
-            description: "",
-            preparationTime: 10,
-            servings: 1,
-        },
-    });
-
     const handleSubmitForm: SubmitHandler<RecipeFormData> = async (values) => {
         try {
             await fetch("/api/recipes", {
@@ -39,34 +29,53 @@ export function CreateRecipeForm() {
     };
 
     return (
-        <Form onSubmit={formApi.handleSubmit(handleSubmitForm)}>
-            <TextInput
-                control={formApi.control}
-                isRequired
-                label="Název"
-                name="title"
-                type="text"
-            />
-            <TextInput
-                control={formApi.control}
-                label="Popisek"
-                name="description"
-                type="text"
-            />
-            <NumberInput
-                control={formApi.control}
-                label="Počet porcí"
-                minValue={1}
-                name="servings"
-            />
-            <NumberInput
-                control={formApi.control}
-                description="Čas v minutách"
-                label="Doba přípravy"
-                minValue={1}
-                name="preparationTime"
-            />
-            <Button type="submit">Odeslat</Button>
+        <Form
+            className="mt-10"
+            onSubmit={handleSubmitForm}
+            options={{
+                defaultValues: {
+                    title: "",
+                    description: "",
+                    preparationTime: 10,
+                    servings: 1,
+                },
+            }}
+            schema={FORM_SCHEMA}
+        >
+            {({ control, formState }) => (
+                <>
+                    <TextInput
+                        control={control}
+                        isRequired
+                        label="Název"
+                        name="title"
+                        type="text"
+                    />
+                    <TextInput
+                        control={control}
+                        isRequired
+                        label="Popisek"
+                        name="description"
+                        type="text"
+                    />
+                    <NumberInput
+                        control={control}
+                        label="Počet porcí"
+                        minValue={1}
+                        name="servings"
+                    />
+                    <NumberInput
+                        control={control}
+                        description="Čas v minutách"
+                        label="Doba přípravy"
+                        minValue={1}
+                        name="preparationTime"
+                    />
+                    <Button isPending={formState.isSubmitting} type="submit">
+                        Odeslat
+                    </Button>
+                </>
+            )}
         </Form>
     );
 }
